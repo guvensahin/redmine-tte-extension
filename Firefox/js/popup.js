78e2c9b9e0f938dd940408823517fa48a739282d
guvensahin <guvensahin@outlook.com>
2021-04-21 01:53:19 +0300

    firefox version deleted.

--- Firefox/js/popup.js
+++
@@ -1,7 +0,0 @@
-// Güven Şahin - guvensahin.com
-// Edit for Firefox Semih Çelikol - semihcelikol.com
-
-browser.storage.local.get(null, function (items) {
-    redmineHelper.getTimeEntriesForPopup(items.redmineUrl, items.redmineApiKey, items.redmineUserId);
-});
-
